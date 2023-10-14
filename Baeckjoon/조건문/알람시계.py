dbcb53f2d49f03888f398ec37a781e403e02ae77
dwd0258 <dwd0258@naver.com>
2023-10-14 21:31:14 +0900

    20231014

--- Baeckjoon/조건문/알람시계.py
+++ Baeckjoon/조건문/알람시계.py
@@ -5,7 +5,23 @@ input = sys.stdin.readline
 
 H,M = map(int,input().split())
 
-if M > 45: 
-    print( H, M-45)
-else :
-     
+'''
+1. (0 <= H <= 24) and ( M >= 45) 
+print(H, M-45) 
+
+2. (0 < H <= 24) and (M < 45)
+print(H-1,(60+M)-45)
+
+3. H == 0 and ( M >= 45)  >> 1번 때문에 없어도됨
+print(H, M-45)
+
+4. H == 0 and (M <45)
+print(23,(60+M)-45)
+'''
+
+if (0 <= H <= 24) and ( M >= 45) : 
+    print(H, M-45) 
+elif (0 < H <= 24) and (M < 45):
+    print(H-1,(60+M)-45)
+elif H == 0 and (M <45):
+    print(23,(60+M)-45)
