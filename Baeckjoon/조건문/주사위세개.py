1cb4321d48fee6a20d85167f546e1a0c745914b9
dwd0258 <dwd0258@naver.com>
2023-10-18 00:42:05 +0900

    20231018

--- Baeckjoon/조건문/주사위세개.py
+++ Baeckjoon/조건문/주사위세개.py
@@ -4,7 +4,15 @@ import sys
 input = sys.stdin.readline
 
 A,B,C = map(int,input().split())
+list = [A,B,C]
 
-if A==B==C :
-    print(10000+(A*B))
-    elif 
+if A==B==C:
+    print(10000+(A*1000))
+elif A==B!=C:
+    print(1000+(A*100))
+elif A!=B==C:
+    print(1000+(C*100))
+elif A==C!=B:
+    print(1000+(A*100))
+elif A!=C!=B:
+    print(max(list)*100)
