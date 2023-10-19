095799bcf30f9c0c42f117cf2854af7b1fb944dd
dwd0258 <dwd0258@naver.com>
2023-10-19 20:13:46 +0900

    20231019

---
+++ Baeckjoon/반복문/구구단.py
@@ -0,0 +1,6 @@
+#2739번
+
+import sys
+input = sys.stdin.readline
+
+n= int(input())
--- Baeckjoon/조건문/주사위세개.py
+++ Baeckjoon/조건문/주사위세개.py
@@ -16,3 +16,19 @@ elif A==C!=B:
     print(1000+(A*100))
 elif A!=C!=B:
     print(max(list)*100)
+    
+'''
+A,B,C = map(int,input().split())
+
+if A==B==C:
+    print(10000+(A*1000))
+elif A==B!=C:
+    print(1000+(A*100))
+elif A!=B==C:
+    print(1000+(C*100))
+elif A==C!=B:
+    print(1000+(A*100))
+elif A!=C!=B:
+    print(max(A,B,C)*100)
+    
+'''
