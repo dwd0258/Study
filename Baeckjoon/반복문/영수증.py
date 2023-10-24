aba7f519f6a08320f3eb5e37743dffe249ce6d44
dwd0258 <dwd0258@naver.com>
2023-10-24 10:06:54 +0900

    20231024

---
+++ Baeckjoon/반복문/영수증.py
@@ -0,0 +1,17 @@
+#25304번
+
+import sys
+input = sys.stdin.readline
+
+x = int(input())
+n = int(input())
+sum=0
+
+for i in range(1,n+1) :
+    a,b = map(int,input().split())
+    sum += a*b
+
+if x == sum :
+    print("Yes")
+else:
+    print("No")
