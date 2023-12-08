a19b5cf799a28b122ecad0b292c677bdcbf7f5d5
dwd0258 <dwd0258@naver.com>
2023-12-08 19:38:15 +0900

    20231208

---
+++ Baeckjoon/반복문/10951번.py
@@ -0,0 +1,12 @@
+#10951번
+#A+B-4
+# EOF 문제 !!
+import sys
+input = sys.stdin.readline
+
+while(1) :
+    try :
+        a,b = map(int,input().split())
+        print(a+b)
+    except :
+        break
